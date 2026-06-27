5287c7b42b72f53805ab1ff6ab7953d97f2c628f
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-06-27 17:28:25 -0300

    feat: estiliza listagem com mat-table e cards no mobile

--- web/src/app/veiculos/veiculo-lista/veiculo-lista.ts
+++ web/src/app/veiculos/veiculo-lista/veiculo-lista.ts
@@ -2,11 +2,15 @@ import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { BehaviorSubject, switchMap } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { VeiculoService } from '../veiculo-service';
 
 @Component({
   selector: 'app-veiculo-lista',
-  imports: [AsyncPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink, MatTableModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './veiculo-lista.html',
   styleUrl: './veiculo-lista.scss'
 })
@@ -18,6 +22,8 @@ export class VeiculoLista {
   protected readonly veiculos$ = this.recarregar$.pipe(
     switchMap(() => this.service.listar())
   );
+
+  protected readonly colunas = ['placa', 'marca', 'modelo', 'ano', 'chassi', 'renavam', 'acoes'];
 
   protected excluir(id: string): void {
     if (!confirm('Tem certeza que deseja excluir este veículo?')) {
