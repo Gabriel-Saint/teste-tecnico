6c2abfbe9029f6ec92f0fae54e618a0149d4fb3c
Gabriel-Saint <gabrielsantosayres@gmail.com>
2026-06-28 11:39:01 -0300

    feat: toolbar com marca clicavel e centralizada

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconRegistry } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   constructor() {
+    // faz todo <mat-icon> usar a fonte Material Symbols Outlined
     inject(MatIconRegistry).setDefaultFontSetClass('material-symbols-outlined');
   }
 }
